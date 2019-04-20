7964c3c8d0b4a8aece8a8c70fd9d9b0e5bff10ec
Bartłomiej Olech <olech.bartlomiej@gmail.com>
2019-04-20 14:35:48 +0200

    improve a view of the code in the app

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,25 @@
-let isHidden=true;
+let isHidden = true;
 document.querySelector('#button-section').addEventListener('click', function () {
-    isHidden=!isHidden;
-    if(!isHidden){
+    isHidden = !isHidden;
+    if (!isHidden) {
         document.querySelector('#d-5').classList.add('showMore')
         document.querySelector('#d-6').classList.add('showMore')
         document.querySelector('#d-7').classList.add('showMore')
         document.querySelector('#d-8').classList.add('showMore')
-    }else{
+    } else {
         document.querySelector('#d-5').classList.remove('showMore')
         document.querySelector('#d-6').classList.remove('showMore')
         document.querySelector('#d-7').classList.remove('showMore')
         document.querySelector('#d-8').classList.remove('showMore')
     }
-    if(!isHidden){
-        document.querySelector('.text-button').innerText='Hide';
+    if (!isHidden) {
+        document.querySelector('.text-button').innerText = 'Hide';
         document.querySelector('#arrow').classList.remove('arrow-down')
         document.querySelector('#arrow').classList.add('arrow-up')
-    }else{
-        document.querySelector('.text-button').innerText='Load more'
+    } else {
+        document.querySelector('.text-button').innerText = 'Load more'
         document.querySelector('#arrow').classList.remove('arrow-up')
         document.querySelector('#arrow').classList.add('arrow-down')
     }
-    
+
 });
